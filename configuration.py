45d060fb6f7077e317e4d949530ba1d67fd3756d
Dmitry-Makhankov <157802888+Dmitry-Makhankov@users.noreply.github.com>
2024-04-22 11:46:25 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://e3fade35-652c-4c73-86a5-0be8b5b710bd.serverhub.praktikum-services.ru"
+CREATE_CLIENT_ORDER = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,11 @@
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchtha",
+    "address": "Konoha, 143 apt.",
+    "metroStation": 4,
+    "phone": "+7 901 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2021-04-06",
+    "comment": "Saske, come back to Konoha",
+    "color": ["BLACK"]
+}
